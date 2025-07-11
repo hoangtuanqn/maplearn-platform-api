a7c9974904e8da267f90a369edc4c047e3794fed
hoangtuanqn <phamhoangtuanqn@gmail.com>
2025-07-11 08:33:48 +0700

    feat: Add feature forgot and reset password

---
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Password;
+
+class ForgotPasswordController extends Controller
+{
+    public function sendResetLink(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+        ]);
+
+        $status = Password::sendResetLink(
+            $request->only('email')
+        );
+        if ($status !== Password::RESET_LINK_SENT) {
+            // Giả lập trạng thái loading => Tránh hacker check email đã tồn tại
+            sleep(4);
+        }
+        return response()->json([
+            'message' => 'Hãy kiểm tra email của bạn. Nếu địa chỉ đã đăng ký, chúng tôi sẽ gửi liên kết đặt lại mật khẩu ngay lập tức.'
+        ]);
+    }
+}
---
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Password;
+
+class ResetPasswordController extends Controller
+{
+    public function resetPassword(Request $request)
+    {
+        $request->validate([
+            'token' => 'string|required',
+            'email' => 'required|email',
+            'password' => 'required|min:6|confirmed',
+        ]);
+
+        $status = Password::reset(
+            $request->only('email', 'password', 'password_confirmation', 'token'),
+            function ($user, $password) {
+                $user->forceFill([
+                    'password' => $password,
+                ])->save();
+            }
+        );
+
+        if ($status === Password::PASSWORD_RESET) {
+            return response()->json(['message' => __($status)]);
+        }
+
+        return response()->json(['message' => __($status)], 400);
+    }
+}
